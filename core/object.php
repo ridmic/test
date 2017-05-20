5985b775db01223e22a7267285701de11521bfed
ridmic <ridmic@gmail.com>
2017-05-21 00:05:20 +1000

    check in

--- core/object.php
+++ core/object.php
@@ -1,22 +1,31 @@
 <?php
+namespace Ridmic\Core;
 
-class xObject
+class Object
 {
-    static       $defLevel   = 1; 
-    static       $defMode    = 'HTML';
-    protected    $dbgLevel   = 0;
+    static       $defLevel     = 1; 
+    static       $defShowClass = false;
     
-    const        DBG_ALWAYS  = 0;
-    const        DBG_DEBUG   = 1;
-    const        DBG_TRACE   = 2;
+    const        DBG_ALWAYS    = 0;
+    const        DBG_DEBUG     = 1;
+    const        DBG_TRACE     = 2;
     
     function __construct()
     {
-        $this->dbgLevel = self::$defLevel;
+      $this->traceFunction();
     }
+    function __destruct()
+    {
+      $this->traceFunction();
+    }
+    
     static function defDebugLevel( $level )
     {
-        self::$defLevel = intval($level );
+      self::$defLevel = intval($level );
+    }
+    static function defShowClass( $b=true )
+    {
+      self::$defShowClass = $b === true;
     }
     
     public function write( $msg ) { $this->_write($msg, self::DBG_ALWAYS); }
@@ -24,22 +33,23 @@ class xObject
     public function trace( $msg ) { $this->_write($msg, self::DBG_TRACE); }
     public function _write( $msg, $level=1 )
     {
-        if ( $level <= $this->dbgLevel )
-        {
-            switch ( self::$defMode )
-            {
-                case 'HTML':
-                    print "<b>DBG".$level."[<i>".__CLASS__."</i>]</b>: $msg<br>\n";    
-                    break;
-                default:
-                    print "DBG".$level."[".__CLASS__."]: $msg\n";
-                    break;
-            }
-        }
-    }  
-    public function setDebugLevel( $level=1 )
+      if ( $level <= self::$defLevel )
+      {
+          if ( self::$defShowClass )
+            print "<b>[".get_class($this)."]</b>: $msg<br>\n";
+          else
+            print "$msg<br>";
+      }
+    } 
+    
+    public function traceFunction()
     {
-        $this->dbgLevel = intval($level);
+      $b = debug_backtrace(false,2);
+      $f = count($b) >= 2 ? $b[1]['function'] : '-unknown-';
+      $v = count($b) >= 2 ? $b[1]["args"] : array();
+          
+      array_walk( $v, function(&$value, $key) { $value = print_r($value, true); } );
+      $this->trace( get_class($this)."::$f(".implode(', ', $v).")" );
     }
 }
 
